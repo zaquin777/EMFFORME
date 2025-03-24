65a3632dff92131e3ae2591f45f6c299b45497a5
esdrasdutra <esdrasfdutra@gmail.com>
2025-03-24 02:11:34 -0300

    Páginas criadas: Cadastro e Sucesso

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,3 @@
-
 from flask import Flask , render_template, request
 
 app = Flask(__name__) 
@@ -10,20 +9,25 @@ alunos = []
 def home():
     return render_template('index.html')
 
-@app.route('/cadastro', methods=['POST'])
+@app.route('/cadastro', methods=['GET','POST'])
 def cadastro():
     nome = request.form.get('nome')
     idade = request.form.get('idade')
-    serie = request.form.get('serie')
     cidade = request.form.get('cidade')
-    
+    endereco = request.form.get('end')
 
-    if nome and idade and serie:
-        aluno = {"nome": nome, "idade": idade, "serie": serie}
-        alunos.append(aluno)
-        return render_template('sucesso.html', aluno=aluno)
-    else:
-        return "Dados inválidos", 400
+    if request.method == 'GET':
+        return render_template('cadastro.html')
+
+    if request.method == 'POST':
+        if nome and idade:
+            aluno = {"nome": nome, "idade": idade, "cidade":cidade, "endereco":endereco}
+            alunos.append(aluno)
+            print(alunos)
+            return render_template('cadastroSucesso.html', aluno=aluno)
+        else:
+            return "Dados inválidos", 400   
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
